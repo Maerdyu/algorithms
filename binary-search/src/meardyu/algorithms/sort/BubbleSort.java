30499b3b8a035631cafcb0a207d4862051f5bf75
Maerdyu <jinchunyu86@outlook.com>
2018-09-27 00:17:08 +0800

    insertion sort

--- binary-search/src/meardyu/algorithms/sort/BubbleSort.java
+++ binary-search/src/meardyu/algorithms/sort/BubbleSort.java
@@ -14,7 +14,7 @@ public class BubbleSort {
 
 	}
 
-	//Good
+	// Good
 	void sortSecond(int arr[]) {
 		int n = arr.length;
 		for (int i = 0; i < n - 1; i++) {
@@ -24,18 +24,33 @@ public class BubbleSort {
 					Exchange.swap(arr, j, j + 1);
 					swaped = true;
 				}
-			
+
 			if (!swaped) {
 				break;
 			}
 		}
 	}
 
+	// recursive
+	// Recursive Bubble Sort has no performance/implementation advantages
+	void sortRecursive(int arr[], int n) {
+
+		if (n == 1)
+			return;
+
+		for (int i = 0; i < n - 1; i++)
+			if (arr[i] > arr[i + 1])
+				Exchange.swap(arr, i, i + 1);
+
+		sortRecursive(arr, n - 1);
+	}
+
 	public static void main(String[] args) {
 		BubbleSort bubble = new BubbleSort();
 		int arr[] = { 12, 34, 2, 45, 1, 34, 4, 32, 2, 5334, 523, 4, 5, 32, 1 };
 
-		bubble.sortSecond(arr);
+//		bubble.sortSecond(arr);
+		bubble.sortRecursive(arr, arr.length);
 
 		Print.printArray(arr);
 	}
