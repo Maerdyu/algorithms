cb2924c75d9c9b42d97fa031184b30cc090bfa38
jinch <jinchunyu86@outlook.com>
2020-12-15 23:37:18 +0800

    “两数相加问题”

--- binary-search/src/meardyu/algorithms/binarysearch/BinarySearch.java
+++ binary-search/src/meardyu/algorithms/binarysearch/BinarySearch.java
@@ -1,41 +1,41 @@
-package meardyu.algorithms.binarysearch;
-
-import java.util.Arrays;
-
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
-
-/**
- * 
- * @author maerdyu
- *
- */
-public class BinarySearch {
-
-	// Êý×é±ØÐëÊÇÓÐÐòµÄ
-	public static int rank(int key, int[] a) {
-		int lo = 0;
-		int hi = a.length - 1;
-		while (hi > lo) {
-			int mid = lo + (hi - lo) / 2; // ?: difference (lo+hi)/2
-			if (a[mid] < key) // ? >=
-				lo = mid + 1; // W: +1
-			else if (a[mid] > key)
-				hi = mid - 1;
-			else
-				return mid;
-		}
-		return -1;
-	}
-
-	public static void main(String[] args) {
-		int[] whitelist = new In("tinyW.txt").readAllInts();
-		Arrays.sort(whitelist);
-		while (!StdIn.isEmpty()) {
-			int key = StdIn.readInt();
-			if (rank(key, whitelist) < 0)
-				StdOut.println(key);
-		}
-	}
-}
+//package meardyu.algorithms.binarysearch;
+//
+//import java.util.Arrays;
+//
+//import edu.princeton.cs.algs4.In;
+//import edu.princeton.cs.algs4.StdIn;
+//import edu.princeton.cs.algs4.StdOut;
+//
+///**
+// *
+// * @author maerdyu
+// *
+// */
+//public class BinarySearch {
+//
+//	// ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+//	public static int rank(int key, int[] a) {
+//		int lo = 0;
+//		int hi = a.length - 1;
+//		while (hi > lo) {
+//			int mid = lo + (hi - lo) / 2; // ?: difference (lo+hi)/2
+//			if (a[mid] < key) // ? >=
+//				lo = mid + 1; // W: +1
+//			else if (a[mid] > key)
+//				hi = mid - 1;
+//			else
+//				return mid;
+//		}
+//		return -1;
+//	}
+//
+//	public static void main(String[] args) {
+//		int[] whitelist = new In("tinyW.txt").readAllInts();
+//		Arrays.sort(whitelist);
+//		while (!StdIn.isEmpty()) {
+//			int key = StdIn.readInt();
+//			if (rank(key, whitelist) < 0)
+//				StdOut.println(key);
+//		}
+//	}
+//}
